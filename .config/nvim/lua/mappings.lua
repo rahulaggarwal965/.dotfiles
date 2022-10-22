65373b3c61b7d023fec72812e47d5498f42ed587
Rahul Aggarwal <rahulaggarwal965@gmail.com>
2022-10-22 16:06:14 -0400

    map -> vim.keymap.set

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,41 +1,39 @@
-local map = vim.keymap.set
-
 vim.g.mapleader = ' '
 
 -- use jk to escape insert mode
-map ("i", "jk", "<Esc>" )
+vim.keymap.set("i", "jk", "<Esc>")
 
 -- hold visual mode when indenting
-map ("x", "<", "<gv")
-map ("x", ">", ">gv")
+vim.keymap.set("x", "<", "<gv")
+vim.keymap.set("x", ">", ">gv")
 
 -- navigate by visual lines rather than normal lines
-map ({"n", "x"}, "j", "gj")
-map ({"n", "x"}, "k", "gk")
+vim.keymap.set({"n", "x"}, "j", "gj")
+vim.keymap.set({"n", "x"}, "k", "gk")
 
 -- move selected block in visual mode
-map ("x", "J", ":m'>+1<CR>='[gv",  { silent = true })
-map ("x", "K", ":m '<-2<CR>='[gv", { silent = true })
+vim.keymap.set("x", "J", ":m'>+1<CR>='[gv",  { silent = true })
+vim.keymap.set("x", "K", ":m '<-2<CR>='[gv", { silent = true })
 
 -- center cursor when moving through search results, but keep counter
-map ("n", "n", "nzz<BS>n")
-map ("n", "N", "Nzz<Space>N")
+vim.keymap.set("n", "n", "nzz<BS>n")
+vim.keymap.set("n", "N", "Nzz<Space>N")
 
 -- don't pollute the clipboard with "pasted over" text
-map ("x", "p", "pgvy")
+vim.keymap.set("x", "p", "pgvy")
 
 -- quickly replay a macro in the q register
-map ("n", "Q", "@q")
+vim.keymap.set("n", "Q", "@q")
 
 -- write to files wit elevated permissions
-map ("c", "w!!", "execute 'silent! write !sudo tee % >/dev/null' <bar> edit!", { silent = true })
+vim.keymap.set("c", "w!!", "execute 'silent! write !sudo tee % >/dev/null' <bar> edit!", { silent = true })
 
 -- terminal movement
-map ("t", "<Esc><Esc>",  [[<C-\><C-n>]]       )
-map ("t", "C-h",         [[<C-\><C-n><C-w>h]] )
-map ("t", "C-j",         [[<C-\><C-n><C-w>j]] )
-map ("t", "C-k",         [[<C-\><C-n><C-w>k]] )
-map ("t", "C-l",         [[<C-\><C-n><C-w>l]] )
+vim.keymap.set("t", "<Esc><Esc>",  [[<C-\><C-n>]]       )
+vim.keymap.set("t", "C-h",         [[<C-\><C-n><C-w>h]] )
+vim.keymap.set("t", "C-j",         [[<C-\><C-n><C-w>j]] )
+vim.keymap.set("t", "C-k",         [[<C-\><C-n><C-w>k]] )
+vim.keymap.set("t", "C-l",         [[<C-\><C-n><C-w>l]] )
 
-map ("n", "<leader>;r", [[:%s//gc<Left><Left><Left>]])
-map ("x", "<leader>;r", [[:s/\%V/g<Left><Left>]])
+vim.keymap.set("n", "<leader>;r", [[:%s//gc<Left><Left><Left>]])
+vim.keymap.set("x", "<leader>;r", [[:s/\%V/g<Left><Left>]])
