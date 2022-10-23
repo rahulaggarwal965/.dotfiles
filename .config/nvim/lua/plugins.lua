0983537e4a6de77cf4d24b6c5de219d07fa15ae4
Rahul Aggarwal <rahulaggarwal965@gmail.com>
2022-10-23 19:55:27 -0400

    fix indenting in plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -14,138 +14,137 @@ vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile'
 
 return packer.startup(function(use)
 
-        use "wbthomason/packer.nvim"
-        use "nvim-lua/plenary.nvim"
-        use "lewis6991/impatient.nvim"
+    use "wbthomason/packer.nvim"
+    use "nvim-lua/plenary.nvim"
+    use "lewis6991/impatient.nvim"
 
-        use { "lewis6991/gitsigns.nvim",
-            config = function()
-                require("plugins.gitsigns")
-            end,
-        }
+    use { "lewis6991/gitsigns.nvim",
+        config = function()
+            require("plugins.gitsigns")
+        end,
+    }
 
-        use { "neovim/nvim-lspconfig", module = "lspconfig" }
-        use { "ray-x/lsp_signature.nvim", module = "lsp_signature" }
+    use { "neovim/nvim-lspconfig", module = "lspconfig" }
+    use { "ray-x/lsp_signature.nvim", module = "lsp_signature" }
 
-        use { "nvim-lualine/lualine.nvim",
-            config = function()
-                require("plugins.lualine")
-            end
-        }
+    use { "nvim-lualine/lualine.nvim",
+        config = function()
+            require("plugins.lualine")
+        end
+    }
 
-        use { "hrsh7th/nvim-cmp",
-            requires =  {
-                "hrsh7th/cmp-buffer",
-                "hrsh7th/cmp-nvim-lsp",
-                "hrsh7th/cmp-nvim-lua",
-                "hrsh7th/cmp-path",
-                "saadparwaiz1/cmp_luasnip",
-                "kdheepak/cmp-latex-symbols",
-            },
-            config = function()
-                require("plugins.cmp")
-            end,
-        }
+    use { "hrsh7th/nvim-cmp",
+        requires =  {
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-nvim-lua",
+            "hrsh7th/cmp-path",
+            "saadparwaiz1/cmp_luasnip",
+            "kdheepak/cmp-latex-symbols",
+        },
+        config = function()
+            require("plugins.cmp")
+        end,
+    }
 
-        use "L3MON4D3/LuaSnip"
-        use { "windwp/nvim-autopairs",
-            config = function()
-                require("nvim-autopairs").setup()
-                local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-                require("cmp").event:on( 'confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
-            end
-        }
+    use "L3MON4D3/LuaSnip"
+    use { "windwp/nvim-autopairs",
+        config = function()
+            require("nvim-autopairs").setup()
+            local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+            require("cmp").event:on( 'confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
+        end
+    }
 
-        use { "ahmedkhalf/project.nvim",
-            config = function()
-                require("project_nvim").setup({
-                    datapath = vim.fn.stdpath("cache")
-                })
-            end
-        }
+    use { "ahmedkhalf/project.nvim",
+        config = function()
+            require("project_nvim").setup({
+                datapath = vim.fn.stdpath("cache")
+            })
+        end
+    }
 
-        use { "nvim-telescope/telescope.nvim",
-            requires = {
-                { "nvim-telescope/telescope-fzy-native.nvim" }
-            },
-            config = function()
-                require("plugins.telescope")
-            end,
-        }
+    use { "nvim-telescope/telescope.nvim",
+        requires = {
+            { "nvim-telescope/telescope-fzy-native.nvim" }
+        },
+        config = function()
+            require("plugins.telescope")
+        end,
+    }
 
-        use { "numToStr/Comment.nvim",
-            config = function()
-                require("Comment").setup()
-            end,
-            keys = "gc"
-        }
+    use { "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup()
+        end,
+        keys = "gc"
+    }
 
-        use { "mfussenegger/nvim-dap",
-            setup = function()
-                require("plugins.dap").setup()
-            end,
-            config = function()
-                require("plugins.dap").config()
-            end,
-            module = 'dap'
-        }
+    use { "mfussenegger/nvim-dap",
+        setup = function()
+            require("plugins.dap").setup()
+        end,
+        config = function()
+            require("plugins.dap").config()
+        end,
+        module = 'dap'
+    }
 
-        use { "rcarriga/nvim-dap-ui",
-            after = "nvim-dap",
-            config = function()
-                require("dapui").setup()
-            end
-        }
+    use { "rcarriga/nvim-dap-ui",
+        after = "nvim-dap",
+        config = function()
+            require("dapui").setup()
+        end
+    }
 
-        use { "mfussenegger/nvim-dap-python", module = "dap-python" }
+    use { "mfussenegger/nvim-dap-python", module = "dap-python" }
 
-        use { "nvim-treesitter/nvim-treesitter",
-            commit = "4cccb6f",
-            config = function()
-                require("plugins.treesitter")
-            end,
-            run = ":TSUpdate"
-        }
+    use { "nvim-treesitter/nvim-treesitter",
+        commit = "4cccb6f",
+        config = function()
+            require("plugins.treesitter")
+        end,
+        run = ":TSUpdate"
+    }
 
-    	use { "kyazdani42/nvim-tree.lua",
-    	    requires = { "kyazdani42/nvim-web-devicons" },
-    	    config = function()
-    	        require("plugins.tree")
-            end
-        }
+    use { "kyazdani42/nvim-tree.lua",
+        requires = { "kyazdani42/nvim-web-devicons" },
+        config = function()
+            require("plugins.tree")
+        end
+    }
 
-        use {"folke/tokyonight.nvim",
-            config = function()
-                require("plugins.theme")
-            end
-        }
+    use {"folke/tokyonight.nvim",
+        config = function()
+            require("plugins.theme")
+        end
+    }
 
-        use { "iamcco/markdown-preview.nvim",
-            run = function() fn['mkdp#util#install']() end,
-            ft = "markdown"
-        }
+    use { "iamcco/markdown-preview.nvim",
+        run = function() fn['mkdp#util#install']() end,
+        ft = "markdown"
+    }
 
-        use { "preservim/vim-markdown" }
+    use { "preservim/vim-markdown" }
 
-        use { "lervag/vimtex", ft = "tex" }
+    use { "lervag/vimtex", ft = "tex" }
 
-        use { "norcalli/nvim-colorizer.lua",
-            cmd = "ColorizerToggle"
-        }
+    use { "norcalli/nvim-colorizer.lua",
+        cmd = "ColorizerToggle"
+    }
 
-        use { "numToStr/Navigator.nvim",
-            config = function()
-                local navigator = require("Navigator")
-                navigator.setup()
+    use { "numToStr/Navigator.nvim",
+        config = function()
+            local navigator = require("Navigator")
+            navigator.setup()
 
-                vim.keymap.set("n", "<C-h>", navigator.left)
-                vim.keymap.set("n", "<C-j>", navigator.down)
-                vim.keymap.set("n", "<C-k>", navigator.up)
-                vim.keymap.set("n", "<C-l>", navigator.right)
-            end
-        }
+            vim.keymap.set("n", "<C-h>", navigator.left)
+            vim.keymap.set("n", "<C-j>", navigator.down)
+            vim.keymap.set("n", "<C-k>", navigator.up)
+            vim.keymap.set("n", "<C-l>", navigator.right)
+        end
+    }
 
-        use "RyanMillerC/better-vim-tmux-resizer"
-    end
-)
+    use "RyanMillerC/better-vim-tmux-resizer"
+end)
 
