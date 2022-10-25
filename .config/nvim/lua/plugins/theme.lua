8e64f69523c88914ccb57511acb4ceda5a5db37e
Rahul Aggarwal <rahulaggarwal965@gmail.com>
2022-10-25 03:05:07 -0400

    add tokyonight, add bash lsp

---
+++ .config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,11 @@
+local present, theme = pcall(require, "tokyonight")
+if not present then
+    return
+end
+
+theme.setup {
+    style = "night",
+    sidebars = {"qf", "help", "packer"}
+}
+
+vim.cmd("colorscheme tokyonight")
