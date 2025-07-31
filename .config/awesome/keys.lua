5c6fcfc1bbc7fd8720f525e5468addcfe1489772
Rahul Aggarwal <rahulaggarwal965@gmail.com>
2025-07-31 09:44:40 -0700

    [awesome] add function for jump_to_or_spawn and outlook shortcut

--- .config/awesome/keys.lua
+++ .config/awesome/keys.lua
@@ -9,6 +9,16 @@ local shift = "Shift"
 local terminal = os.getenv("TERMINAL")
 local browser = os.getenv("BROWSER")
 
+local function jump_to_or_spawn(instance, command)
+    for _, c in ipairs(client.get()) do
+        if c.instance:match(instance) then
+            c:jump_to()
+            return
+        end
+    end
+    awful.spawn(command or instance)
+end
+
 awful.keyboard.append_global_keybindings({
     awful.key({super, shift}, "Escape", awesome.quit),
     awful.key({super, shift}, "w", awesome.restart),
@@ -98,15 +108,10 @@ awful.keyboard.append_global_keybindings({
     awful.key({super, alt}, "b", function() awful.spawn(browser, false) end),
     awful.key({super, alt}, "d", function() awful.spawn("discord", false) end),
     awful.key({super, alt}, "l", function() awful.spawn("slack", false) end),
-    awful.key({super, alt}, "s", function()
-        for _, c in ipairs(client.get()) do
-            if c.instance:match("spotify") then
-                c:jump_to()
-                return
-            end
-        end
-        awful.spawn("spotify")
+    awful.key({super, alt}, "o", function()
+        jump_to_or_spawn("outlook.office.com__mail", "google-chrome --app=https://outlook.office.com/mail/ --class=ICE-SSB-outlook --user-data-dir=/home/raaggarwal/.local/share/pwa/profiles/outlook")
     end),
+    awful.key({super, alt}, "s", function() jump_to_or_spawn("spotify") end),
 
     awful.key({super, alt}, "h", function() awful.spawn(terminal.. " -e btop", false) end),
     awful.key({super, alt}, "p", function() awful.spawn(terminal.. " -e pulsemixer", false) end)
